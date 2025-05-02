0abd6d15ab01f9ef2b170200cc3d5d8ef9af4284
Akmal <farhanakmal@student.telkomuniversity.ac.id>
2025-05-03 06:57:38 +0700

    Update Progress

---
+++ ML Project/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='mlproject',
+    version='0.0.1'
+    author='akmal',
+    packages=find_packages(),
+    install_require=['pandas', 'numpy', 'seaborn']
+    
+)
